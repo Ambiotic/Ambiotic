c088d6d60942b1f7e273c2f4f209045085b0799f
jim <jim.carreer@gmail.com>
2015-09-30 19:26:51 -0400

    Use gradle to set version

--- src/main/java/graphich/ambiotic/main/Ambiotic.java
+++ src/main/java/graphich/ambiotic/main/Ambiotic.java
@@ -32,7 +32,7 @@ public class Ambiotic {
 
     public static final String MODID = "ambiotic";
     public static final String NAME = "Ambiotic";
-    public static final String VERSION = "0.0.3";
+    public static final String VERSION = "@VERSION@";
 
     protected static Logger logger;
     public static Logger logger() {
