bdf3970cf757f491c6fa34ef78fb1d4fd66ae5e2
jim <jim.carreer@gmail.com>
2015-10-22 07:55:37 -0400

    Biome Tag Counter

---
+++ src/main/java/graphich/ambiotic/variables/special/BiomeTagCounter.java
@@ -0,0 +1,43 @@
+package graphich.ambiotic.variables.special;
+
+import com.google.gson.annotations.SerializedName;
+import cpw.mods.fml.common.gameevent.TickEvent;
+import graphich.ambiotic.scanners.BlockScanner;
+import graphich.ambiotic.util.StrictJsonException;
+import graphich.ambiotic.variables.VariableScanning;
+import net.minecraftforge.common.BiomeDictionary;
+
+public class BiomeTagCounter extends VariableScanning {
+
+    @SerializedName("Tag")
+    protected BiomeDictionary.Type mTag;
+
+    public BiomeTagCounter(String name, BlockScanner scanner) {
+        super(name, scanner);
+        initialize();
+    }
+
+    @Override
+    public void validate() throws StrictJsonException {
+        super.validate();
+        if(mTag == null)
+            throw new StrictJsonException("Tag is required and must be one of "+tagnames);
+    }
+
+    @Override
+    public boolean updateValue(TickEvent event) {
+        return false;
+    }
+
+    protected static String tagnames;
+    static
+    {
+        tagnames = null;
+        for(BiomeDictionary.Type type : BiomeDictionary.Type.values()) {
+            if(tagnames == null)
+                tagnames = type.name();
+            else
+                tagnames += ", "+type.name();
+        }
+    }
+}
